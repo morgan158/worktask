d129d9f7a9fb6b3df996e89a326bed636abdddb4
Svetlana Astapova <s.astapova@yoursolution.ru>
2023-03-26 15:49:06 +0500

    start

---
+++ src/test/java/com/test/worktask/WorktaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.worktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
